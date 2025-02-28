98824e57ccaa672123bc729ecc98dffc03d6d3fc
Donis03ch <111046453+Donis03ch@users.noreply.github.com>
2025-02-28 10:23:38 +0100

    Add files via upload

---
+++ Poker/Classes/Computer.cs
@@ -0,0 +1,31 @@
+﻿using CasGames.Poker.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CasGames.Poker.Classes
+{
+    class Computer : Player
+    {
+        #region Constructors
+        public Computer(List<string> names)
+        {
+            Name = SetComputerName(names);
+        }
+        #endregion
+
+        #region Methods
+        private string SetComputerName(List<string> names)
+        {
+            Random rand = new Random();
+            string name = names[rand.Next(0, names.Count)];
+            names.Remove(name);  // To avoid duplicate computer names.
+
+            return name;
+        }
+        #endregion
+    }
+}
---
+++ Poker/Classes/Human.cs
@@ -0,0 +1,20 @@
+﻿using CasGames.Poker.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CasGames.Poker.Users
+{
+    class Human : Player
+    {
+        #region Constructors
+        public Human(string name, int playerNum)
+        {
+            Name = (name != "") ? name : $"Player {playerNum}";
+        }
+        #endregion
+    }
+}
